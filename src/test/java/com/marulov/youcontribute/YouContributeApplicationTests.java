4a70d2597d7f4930441834cd156c9b980377b79f
Marulov <nazmmarul@hotmail.com>
2022-11-13 17:33:50 +0300

    project started.

---
+++ src/test/java/com/marulov/youcontribute/YouContributeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marulov.youcontribute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouContributeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

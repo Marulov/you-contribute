207016cf6d3089d8a87aea43215eed4c63c478da
Marulov <nazmmarul@hotmail.com>
2022-11-28 22:16:50 +0300

    project model updated and RestTemplateConfiguration class added.

--- src/main/java/com/marulov/youcontribute/model/Project.java
+++ src/main/java/com/marulov/youcontribute/model/Project.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "projects")
@@ -18,6 +21,16 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String organization;
+
     private String repository;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
 }
